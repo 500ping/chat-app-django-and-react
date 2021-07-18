22661a914dac171935aeb580c2549a84a23b3ab0
500ping <ndt512x@gmail.com>
2021-07-18 15:42:51 +0700

    re-create front-end

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-
-class App extends React.Component {
-    render () {
-        return (
-            <div>Hello</div>
-        )
-    }
-}
-
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(<h1>Hello World!!!</h1>, document.getElementById('root'));
